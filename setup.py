713fee1b10e5a581fc420ad8b4badfbe3ae2316c
Tom Gurion <nagasaki45@gmail.com>
2016-03-24 18:22:18 +0200

    Formaly support python 3 in setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,9 @@ setup(
         "Environment :: Web Environment",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Scientific/Engineering",
     ]
 )
